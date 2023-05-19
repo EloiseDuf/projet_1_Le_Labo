db1590e7b3c2892f8d21f824139f328d42ed1837
Jerome Desouches <jerome.desouches@gmail.com>
2023-05-19 08:35:19 +0200

    insertion à home d'une popup de présentation du site

---
+++ javascript/home.js
@@ -0,0 +1,7 @@
+const buttonPopup = document.getElementById("buttonPopup");
+const popup = document.getElementById("popup");
+
+
+buttonPopup.addEventListener("click", () => {
+    popup.style.display="none";
+})
