4c4dba30c86400fa3dbd949d9378de54db670da7
Jerome Desouches <jerome.desouches@gmail.com>
2023-05-21 20:52:48 +0200

    popup responsive via recup variable javascript

--- javascript/home.js
+++ javascript/home.js
@@ -1,3 +1,38 @@
+
+let largeurWindow=window.innerWidth;
+let hauteurWindow=window.innerHeight;
+
+const setWidthPopup = (largeur,hauteur) => {
+    if(largeur > hauteur){
+        return 0.8 * hauteur + "px";
+    } else {
+        return 0.8 * largeur + "px";
+    }
+}
+
+const setWindowHeight = () => {
+    return hauteurWindow + "px";
+}
+
+const root =document.documentElement;
+root.style.setProperty("--dimensionPopup", setWidthPopup(largeurWindow,hauteurWindow));
+root.style.setProperty("--windowHeight", setWindowHeight()); 
+
+
+const resetWidthPopup = () => {
+    largeurWindow=window.innerWidth;
+    hauteurWindow=window.innerHeight;
+    root.style.setProperty("--dimensionPopup", setWidthPopup(largeurWindow,hauteurWindow));
+    root.style.setProperty("--windowHeight", setWindowHeight()); 
+}
+
+window.onresize=resetWidthPopup;
+
+// console.log(setWidthPopup(largeurWindow,hauteurWindow));
+
+// console.log(largeurWindow);
+
+
 const buttonPopup = document.getElementById("buttonPopup");
 const popup = document.getElementById("popup");
 
