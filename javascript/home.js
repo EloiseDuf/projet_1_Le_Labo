e24d608878420a174577a87b96c7389b6e059eab
Jerome Desouches <jerome.desouches@gmail.com>
2023-05-22 09:55:38 +0200

    ajout commentaires à home.js

--- javascript/home.js
+++ javascript/home.js
@@ -1,7 +1,9 @@
-
+//récupération de la largeur et la hauteur de la fenêntre du navigateur
 let largeurWindow=window.innerWidth;
 let hauteurWindow=window.innerHeight;
 
+
+//fonction renvoyant une largeur de popup en px en fonction de la taille de la fenetre
 const setWidthPopup = (largeur,hauteur) => {
     if(largeur > hauteur){
         return 0.8 * hauteur + "px";
@@ -10,15 +12,18 @@ const setWidthPopup = (largeur,hauteur) => {
     }
 }
 
+//fonction renvoyant la hauteur de la fenetre en px
 const setWindowHeight = () => {
     return hauteurWindow + "px";
 }
 
+//attribution des largeurs et hauteurs de fenêtre à des variables CSS
 const root =document.documentElement;
 root.style.setProperty("--dimensionPopup", setWidthPopup(largeurWindow,hauteurWindow));
 root.style.setProperty("--windowHeight", setWindowHeight()); 
 
 
+// fonction donnant les actions à faire lorsque la fenetre du navigateur est redimensionnée
 const resetWidthPopup = () => {
     largeurWindow=window.innerWidth;
     hauteurWindow=window.innerHeight;
@@ -26,13 +31,13 @@ const resetWidthPopup = () => {
     root.style.setProperty("--windowHeight", setWindowHeight()); 
 }
 
+//on appelle la fonction lorsque la fenêtre est redimensionnée
 window.onresize=resetWidthPopup;
 
-// console.log(setWidthPopup(largeurWindow,hauteurWindow));
-
-// console.log(largeurWindow);
 
 
+
+//on cache le popup lorsqu'on clique sur le bouton
 const buttonPopup = document.getElementById("buttonPopup");
 const popup = document.getElementById("popup");
 
